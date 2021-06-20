b9dbfd7e8390667c94399a27fce7ebc54abc645b
Graham Guthrie <grahamguthrie99@gmail.com>
2021-06-20 17:08:14 -0400

    Removed port

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,4 +9,4 @@ app = connexion.App(__name__, specification_dir='./swagger/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('swagger.yaml', arguments={
             'title': 'Scraper API'}, pythonic_params=True)
-app.run(port=8080)
+app.run()
