de9a5994ddbd7e0282f632d2d9dfef35b3f42dd3
Graham Guthrie <grahamguthrie99@gmail.com>
2021-06-20 17:39:57 -0400

    Added cors support

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,10 +3,13 @@
 import connexion
 
 from swagger_server import encoder
+from flask_cors import CORS
 
 
 app = connexion.App(__name__, specification_dir='./swagger/')
+
 app.app.json_encoder = encoder.JSONEncoder
+CORS(app.app)
 app.add_api('swagger.yaml', arguments={
             'title': 'Scraper API'}, pythonic_params=True)
 if __name__ == '__main__':
